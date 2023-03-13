d85b954d92a5541e2898e6e75bbd3ac57d18d450
Maure Andrade <pedromaure@gmail.com>
2023-03-13 14:46:33 -0300

    fixing .gitignore

--- backend/api/api/urls.py
+++ backend/api/api/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from core.views import CompainhaViewSet, ViagemViewSet, PassagemViewSet, ReservaViewSet, MunicipiosViewSet
+from core.views import CompainhaViewSet, ViagemViewSet, PassagemViewSet, ReservaViewSet, MunicipiosViewSet, ClasseViagemViewSet
 from rest_framework.routers import DefaultRouter
 from drf_yasg.views import get_schema_view as swagger_get_schema_view
 from drf_yasg import openapi
@@ -24,6 +24,7 @@ router.register(r'viagem', ViagemViewSet, basename='viagem')
 router.register(r'passagem', PassagemViewSet, basename='passagem')
 router.register(r'reserva', ReservaViewSet, basename='reserva')
 router.register(r'municipios', MunicipiosViewSet, basename='municipios')
+router.register(r'classe', ClasseViagemViewSet, basename='classe')
 
 
 urlpatterns = [

2db8a722630846e9b12f01eecc83739864b0eef7
Israel Kleber <israel.lima@escolar.ifrn.edu.br>
2023-03-14 14:30:52 -0300

    Início da tela de reserva de passagem

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,16 +4,18 @@ import ListarDePassagem from "./components/ListarDePassagem"
 import CadastrarCompanhia from "./pages/CadastrarCompanhia"
 import CadastrarViagem from "./pages/CadastrarViagem"
 import ReservaDePassagem from "./pages/ReservaDePassagem"
+import TelaInicial from "./pages/TelaInicial"
 
 
 function App() {
   return (
     <>
       <Cabecalho />
-      <CadastrarViagem />
-      {/* <ReservaDePassagem /> */}
+      {/* <CadastrarViagem /> */}
+      {/* <TelaInicial /> */}
       {/* <ListarDePassagem /> */}
       {/* <CadastrarCompanhia /> */}
+      <ReservaDePassagem />
     </>
   )
 }

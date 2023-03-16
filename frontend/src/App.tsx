7640a28ecd17e1718e35a80a0a0da15160e25912
Maure Andrade <maure.andrade@escolar.ifrn.edu.br>
2023-03-16 13:59:23 -0300

    Adicionado react router dom ao projeto

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,18 +4,20 @@ import ListarDePassagem from "./components/ListarDePassagem"
 import CadastrarCompanhia from "./pages/CadastrarCompanhia"
 import CadastrarViagem from "./pages/CadastrarViagem"
 import ReservaDePassagem from "./pages/ReservaDePassagem"
+import { Routes, Route, Link } from 'react-router-dom'
 import TelaInicial from "./pages/TelaInicial"
-
 
 function App() {
   return (
     <>
       <Cabecalho />
-      {/* <CadastrarViagem /> */}
-      {/* <TelaInicial /> */}
-      {/* <ListarDePassagem /> */}
-      {/* <CadastrarCompanhia /> */}
-      <ReservaDePassagem />
+      <Routes>
+        <Route path="/" element={<TelaInicial/>}></Route>
+        <Route path="/reserva-viagem" element={<ReservaDePassagem/>}></Route>
+        <Route path="/cadastrar-viagem" element={<CadastrarViagem/>}></Route>
+        <Route path="/listar-passagens" element={<ListarDePassagem/>}></Route>
+        <Route path="/cadastrar-companhia" element={<CadastrarCompanhia/>}></Route>
+      </Routes>
     </>
   )
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'react-bootstrap-typeahead/css/Typeahead.css';
+import { BrowserRouter } from 'react-router-dom';
 
+import './index.css'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 )
